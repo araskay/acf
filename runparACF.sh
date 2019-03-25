c7e79544eea227485690b2248ecb91144bda82d6
Aras K <arasmedim@outlook.com>
2019-03-25 14:59:00 +0000

    Deleted runparACF.sh

--- runparACF.sh
+++
@@ -1,6 +0,0 @@
-\cd /home/achemparathy/PHANTOM_DATA/PHA_detrendedAFNIsmoothed7mm/
-scans=(detrended*.nii)
-for scan in ${scans[@]}
-do
- qsub /home/achemparathy/PHANTOM_DATA/detrendedAFNIsmoothed7mm.sh $scan
-done
